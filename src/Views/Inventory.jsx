725396dc7a4a03665a382594dd29f28940210b7b
Bahbi Lee <superbahbi@users.noreply.github.com>
2020-04-04 16:39:34 -0700

    Refactor inventory page

--- src/Views/Inventory.jsx
+++ src/Views/Inventory.jsx
@@ -1,25 +1,11 @@
 import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
-import Navbar from ".././Components/Navbar";
-import Table from ".././Components/Table";
-import styled from "styled-components";
-const DashboardContainer = styled.main`
-  position: relative;
-  overflow: hidden;
-  transition: all 0.15s;
-  padding: 0 20px;
-  margin-left: ${props => (props.expanded ? 240 : 85)}px;
-`;
-const Content = styled.div`
-  height: 100vh
-  background-color: #f8f9fc;
-`;
-
+import CustomPage from "../Components/CustomPage";
 class Inventory extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "Inventory",
+      page: "Inventory",
       rows: [],
       columns: [
         {
@@ -51,33 +37,10 @@ class Inventory extends Component {
     };
   }
 
-  // componentWillMount() {}
-
-  // componentDidMount() {}
-
-  // componentWillReceiveProps(nextProps) {}
-
-  // shouldComponentUpdate(nextProps, nextState) {}
-
-  // componentWillUpdate(nextProps, nextState) {}
-
-  // componentDidUpdate(prevProps, prevState) {}
-
-  // componentWillUnmount() {}
-
   render() {
     return (
       <Fragment>
-        <Content>
-          <Navbar />
-          <DashboardContainer expanded={this.state.expanded}>
-            <Table
-              title={this.state.title}
-              columns={this.state.columns}
-              products={this.state.rows}
-            />
-          </DashboardContainer>
-        </Content>
+        <CustomPage page={this.state.page} columns={this.state.columns} />
       </Fragment>
     );
   }
