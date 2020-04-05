1f0dbf6f409dffed473b556d65545aa75cc8974f
Bahbi Lee <superbahbi@users.noreply.github.com>
2020-04-05 16:09:25 -0700

    Update fetch.service.js

--- src/_services/fetch.service.js
+++ src/_services/fetch.service.js
@@ -1,18 +1,16 @@
 import { authenticationService } from "../_services";
 export const fetchService = {
-  getAllData,
-  addData,
-  editData,
-  deleteData
+  fetchDataFromDB
 };
 
-function getAllData(db) {
+function fetchDataFromDB(db, method, body) {
   const requestOptions = {
-    method: "GET",
+    method: method,
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + authenticationService.currentUser.token
-    }
+    },
+    body: JSON.stringify(body)
   };
 
   return fetch(
@@ -20,56 +18,5 @@ function getAllData(db) {
     requestOptions
   ).then(response => {
     return response;
-  });
-}
-function addData(db, body) {
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + authenticationService.currentUser.token
-    },
-    body: JSON.stringify(body)
-  };
-
-  return fetch(
-    `${process.env.REACT_APP_API_URL}/v1/${db}/add`,
-    requestOptions
-  ).then(batch => {
-    return batch;
-  });
-}
-function editData(db, body) {
-  const requestOptions = {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + authenticationService.currentUser.token
-    },
-    body: JSON.stringify(body)
-  };
-
-  return fetch(
-    `${process.env.REACT_APP_API_URL}/v1/${db}/edit`,
-    requestOptions
-  ).then(batch => {
-    return batch;
-  });
-}
-function deleteData(db, body) {
-  const requestOptions = {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + authenticationService.currentUser.token
-    },
-    body: JSON.stringify(body)
-  };
-
-  return fetch(
-    `${process.env.REACT_APP_API_URL}/v1/${db}/delete`,
-    requestOptions
-  ).then(batch => {
-    return batch;
   });
 }
